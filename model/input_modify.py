8415ec90c801a0a04f19483543c08f27c93eab67
Tina <tina.zhou.hui@gmail.com>
2020-10-19 20:10:22 +0200

    + takes out the csv reading

--- model/input_modify.py
+++ model/input_modify.py
@@ -6,11 +6,10 @@ def convert_to_utf(path):
     data.to_csv(path, encoding='utf-8', index=False)
 
 
-def get_book_info(book_title: str) -> object:
+def get_book_info(books: object, book_title: str) -> object:
     """Takes the exact name of the book title and gives back the most popular ISBN"""
 
-    books_popularity = pd.read_csv("../input/books_popularity.csv", encoding='utf-8')
-    books_with_same_title = books_popularity[books_popularity['title'].str.contains(book_title)]
+    books_with_same_title = books[books['title'].str.contains(book_title)]
     sorted_by_popularity = books_with_same_title.sort_values(by=['popularity'], ascending=False)
 
     return sorted_by_popularity.iloc[0]
--- tests/input_modify.py
+++ tests/input_modify.py
@@ -1,14 +1,18 @@
 import unittest
+import pandas as pd
 
 from model.input_modify import get_book_info
 
+books_popularity = pd.read_csv("../input/books_popularity.csv", encoding='utf-8')
+
 
 class TestDummy(unittest.TestCase):
+
     def test_setting_environment(self):
         self.assertEqual(1, 1, 'environment not set up')
 
     def test_get_book_info(self):
         title = 'The Fellowship of the Ring'
-        output = get_book_info(title)
+        output = get_book_info(books_popularity, title)
 
         self.assertEqual('0345339703', output.ISBN)
